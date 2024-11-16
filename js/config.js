74422a1fe392b00991cad1f10702afba6b6c2873
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2024-11-15 22:40:08 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-
+// js/config.js
+const CONFIG = {
+    transportModes: {
+        car: { name: 'Car', carbonPerMile: 404 },
+        electricCar: { name: 'Electric Car', carbonPerMile: 100 },
+        motorcycle: { name: 'Motorcycle', carbonPerMile: 280 },
+        scooter: { name: 'Scooter', carbonPerMile: 50 },
+        walking: { name: 'Walking', carbonPerMile: 0 },
+        bicycle: { name: 'Bicycle', carbonPerMile: 0 }
+    },
+    apis: {
+        weather: 'YOUR_WEATHER_API_KEY',
+        airQuality: 'YOUR_AIRNOW_API_KEY',
+        routing: 'YOUR_OPENROUTE_API_KEY'
+    }
+};
